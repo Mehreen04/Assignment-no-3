e357b92b554dcbf9c73ba9ca21a2d7cd21ea3544
Mehreen04 <152015996+Mehreen04@users.noreply.github.com>
2023-11-25 19:54:19 +0500

    Add files via upload

---
+++ mArray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "mArray.h"
+using namespace std;
+int main()
+{
+	int size,else;
+	cout<<"Enter the number of elements in your array:";
+	cin>>size;
+	int array[size];
+	cout<<"input the element:";
+	cout<<arr::inputarray(array,size);
+	cout<<"Sortes array:";
+	cout<<arr::bubblesort(array,size);
+	cout<<"Search for element:";
+	cout<<arr::binarySearch(array,size,etc);
+	
+}
