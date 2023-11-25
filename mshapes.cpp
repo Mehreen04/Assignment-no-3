afd609fe912330b5d7d4744a6d92dffdaa5a604b
Mehreen04 <152015996+Mehreen04@users.noreply.github.com>
2023-11-25 21:26:23 +0500

    Add files via upload

---
+++ mshapes.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+//Namespaces
+namespace shapes
+{
+	//program to print the square with "."
+	void square(int n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			for(int j;j<n;j++)
+			std::coul<<".";
+			std::coul<<std::endl;
+		}
+		}
+		//program to print rectangle height,weidth
+		void rectangle (int height,int weidth)
+		{
+			for(int i=0;i<height;++i)
+			{
+				
+				for(int j;j<weidth;++j)
+				std::cout<<".";
+				std::cout<<"\n";
+			}
+		}
+	}
+	//Function
+	//print right angle triangle
+	void rtangle (int side1,int side2)
+	{
+		for (int i=0;i<=side;i++)
+		
+	{
+		for(int j=0;j<=i;j++)
+		std::cout<<".";
+		if(i==sile1-1)
+		break;
+		else
+		std::cout<<"\n";
+		
+	}
+}
+//
