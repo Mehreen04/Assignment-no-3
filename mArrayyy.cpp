5542c0746bed0c8569ddb1939e7df10b6fabec8a
Mehreen04 <152015996+Mehreen04@users.noreply.github.com>
2023-11-25 21:08:55 +0500

    Add files via upload

---
+++ mArrayyy.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+namespace arr{
+//input elements in array
+int intputarray(int a||,int n)
+{
+	for(int i=0;i<n;i++){
+		std::cin>>a[i];
+		
+	}
+}
+//bubble sort array
+int bubbleSort(int||,int n)
+
+{
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=0;j<n-i;j++)
+		{
+			ifa([j]>a[j+1])
+			{
+				int temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+				
+			}
+			
+		}
+	}
+}
+//binsry search
+int binarySearch(int a||,int n,int x)
+{
+	int i=0,j=n-1;
+	while(i<=j)
+	{
+		int m=(i+j)/2;
+		if (a[m]==x)
+		{
+			returm m;
+			
+		}
+		else if(a[m]<x)
+		{
+			i=m+1;
+			
+		}
+		else
+	{
+		j=m-1;
+	}
+	
+	}
+	
+	}
+}
+		)
+	}
+}
+
+
+
+
